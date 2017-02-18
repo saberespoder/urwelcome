4719e8d798879cc9fd9980bf4a8bbbeec4bca967
Alessio Fanelli <fanahova@gmail.com>
2017-02-18 10:57:05 +0100

    Fix database naming

--- rack_config.rb
+++ rack_config.rb
@@ -4,7 +4,7 @@ require 'slim'
 if ENV['RACK_ENV'] == 'production'
   DATABASE_PATH = ENV.fetch('DATABASE_URL')
 else
-  DATABASE_PATH = 'postgres://localhost/drpepper_development'.freeze
+  DATABASE_PATH = 'postgres://localhost/urwelcome_development'.freeze
 end
 
 DB = Sequel.connect(DATABASE_PATH)
